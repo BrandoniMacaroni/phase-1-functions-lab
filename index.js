f0591cbbbb2bae37f98aa22e028816285ee46474
Brandon <bcharris713@gmail.com>
2022-04-29 16:33:53 -0500

    commit tree 9d5e84730d991d5d64d8f2fa5747cba1d6866ec1

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distanceHq) {
+    return Math.abs(42 - distanceHq);
+}
+
+function distanceFromHqInFeet(valueHq) {
+    return (264 * distanceFromHqInBlocks(valueHq));
+}
+
+function distanceTravelledInFeet(start, destination) {
+    let result = Math.abs(start - destination) * 264;
+    return result;
+}
+
+function calculatesFarePrice(start, destination) {
+    let result = (Math.abs(start - destination) * 264);
+    if (result <= 400) {
+        return 0;
+    }
+    if (result <= 2000 && result > 400) {
+        return (((result - 400)* 2)/100);
+    }
+    if (result > 2000 && result < 2500) {
+        return 25;
+    } 
+    if (result > 2500) {
+        return 'cannot travel that far';
+    }
+}
